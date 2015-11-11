174b8942eceb08e33d7658ef28bc39e3202b0264
elital <talkirat@gmail.com>
2015-11-11 13:40:56 +0200

    Load tab is on the left, Load button is implemented.

--- Concord.App/MainTabs/LoadControl.xaml.cs
+++ Concord.App/MainTabs/LoadControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,12 +28,55 @@ namespace Concord.App.MainTabs
 
         private void SaveNewSong_OnClick(object sender, RoutedEventArgs e)
         {
+            var lines = LyricsText.Text.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+
             throw new NotImplementedException();
         }
 
         private void LoadNewSong_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+                {
+                    DefaultExt = ".txt",
+                    Filter = "TEXT Files (*.txt)|*.txt|XML Files (*.xml)|*.xml"
+                };
+
+            var isValid = false;
+
+            while (!isValid)
+            {
+                var dialogResult = openFileDialog.ShowDialog();
+
+                if (!dialogResult.HasValue || !dialogResult.Value)
+                    return;
+
+                var filename = openFileDialog.FileName;
+                string text = string.Empty;
+                MessageBoxResult messageBoxResult = MessageBoxResult.OK;
+
+                try
+                {
+                    text = File.ReadAllText(filename);
+                    isValid = true;
+                }
+                catch (FileNotFoundException)
+                {
+                    messageBoxResult = MessageBox.Show("The chosen file is not found, please choose a different file.", "File not found", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                }
+                catch
+                {
+                    messageBoxResult = MessageBox.Show("An error occured while openning the file, please try again.", "An error occured", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                }
+
+                if (messageBoxResult == MessageBoxResult.Cancel)
+                    return;
+
+                if (isValid)
+                {
+                    LyricsText.Clear();
+                    LyricsText.Text = text;
+                }
+            }
         }
     }
 }
