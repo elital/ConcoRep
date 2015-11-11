d315c3c01e7130b4c9e4e54d0df5cb5f89b89bb5
elital <talkirat@gmail.com>
2015-11-11 10:47:07 +0200

    Change App name (suffix .App)

---
+++ Concord.App/MainTabs/LoadControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Concord.App.MainTabs
+{
+    /// <summary>
+    /// Interaction logic for LoadControl.xaml
+    /// </summary>
+    public partial class LoadControl : TabItem
+    {
+        public LoadControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveNewSong_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadNewSong_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
